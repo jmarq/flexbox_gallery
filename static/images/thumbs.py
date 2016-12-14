475a9c4296f3be50db64c28fbbd573376c7fcf2f
Jimbo <jimbo@jimbo.jim>
2016-12-14 15:00:14 -0600

    correcting thumbs.py

--- static/images/thumbs.py
+++ static/images/thumbs.py
@@ -9,8 +9,21 @@ def isJPG(string):
 
 folder_contents = listdir('.')
 images = filter(lambda(d):isJPG(d),folder_contents)
+img_template = '<img data-slide="%d" class="thumbnail" src="static/images/%s" alt="">\n'
+slide_template = '<div class="slide%d slide"><img alt=""></div>\n'
+imgs_html = ""
+slides_html = ""
+i = 1
 for image in images:
     im = Image.open(image)
     thumb = ImageOps.fit(im,(300,300),Image.ANTIALIAS)
-    thumb.save("thumbs/"+image.replace(".JPG","")+".thumbnail.JPG")
+    thumbnail_filename = "thumbs/"+image.replace(".JPG","")+".thumbnail.JPG"
+    thumb.save(thumbnail_filename)
+    img_html = img_template % (i, thumbnail_filename)
+    slide_html = slide_template % i
+    imgs_html += img_html
+    slides_html += slide_html
+    i += 1
 
+print imgs_html
+print slides_html
